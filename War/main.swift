3fe431e6b26e064ecd2052035f351a80deabe520
Julio Midence <jmidence@lcs.on.ca>
2020-02-12 19:18:47 -0500

    Initial Commit

---
+++ War/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  War
+//
+//  Created by Midence Rosario, Julio on 2020-02-12.
+//  Copyright © 2020 Julio Midence. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
